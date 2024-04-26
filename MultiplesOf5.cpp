0fc58aeff842de7a678831634eaf289f9aa0a5a1
Levy Abreu <148262651+LevyAbreu@users.noreply.github.com>
2024-04-25 22:54:37 -0400

    Create MultiplesOf5.cpp

---
+++ MultiplesOf5.cpp
@@ -0,0 +1,26 @@
+//6) Escreva um algoritmo que armazene em um vetor todos os números múltiplos de 5, no intervalo fechado de 1 a 500. Após isso, o algoritmo deve imprimir todos os valores armazenados.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int tamanho = 100;
+    int vetor[tamanho];
+    int contador = 0;
+
+    for (int i = 1; i <= 500; ++i) {
+        if (i % 5 == 0) {
+            vetor[contador] = i;
+            contador++;
+        }
+    }
+
+    cout << "Valores armazenados no vetor (múltiplos de 5 de 1 a 500):\n";
+    for (int i = 0; i < contador; ++i) {
+        cout << vetor[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
