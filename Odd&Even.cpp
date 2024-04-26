025066f3f1cba19e13780e878da003ba152a9816
Levy Abreu <148262651+LevyAbreu@users.noreply.github.com>
2024-04-25 22:37:49 -0400

    Update Odd&Even.cpp

--- Odd&Even.cpp
+++ Odd&Even.cpp
@@ -5,27 +5,23 @@ using namespace std;
 int main() {
     const int tamanho = 10;
 
-    // Declarando as matrizes A e B
     int A[tamanho];
     int B[tamanho];
 
-    // Leitura dos elementos da matriz A
     cout << "Digite os elementos da matriz A:\n";
     for (int i = 0; i < tamanho; ++i) {
         cout << "Elemento " << i + 1 << ": ";
         cin >> A[i];
     }
 
-    // Construindo a matriz B de acordo com a regra dada
     for (int i = 0; i < tamanho; ++i) {
-        if (i % 2 == 0) { // Se o índice for par
+        if (i % 2 == 0) {
             B[i] = A[i] + 6;
-        } else { // Se o índice for ímpar
+        } else {
             B[i] = A[i] * 6;
         }
     }
 
-    // Exibindo os conteúdos das duas matrizes
     cout << "\nMatriz A:\n";
     for (int i = 0; i < tamanho; ++i) {
         cout << A[i] << " ";
