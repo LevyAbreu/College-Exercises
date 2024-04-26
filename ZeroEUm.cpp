cd46ca933673a6544502634f66abdf966e2bd7ce
Levy Abreu <148262651+LevyAbreu@users.noreply.github.com>
2024-04-25 22:28:49 -0400

    Zero&Um.cpp

---
+++ ZeroEUm.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int tamanho = 30;
+    int vetorOriginal[tamanho];
+    int vetorModificado[tamanho];
+
+    cout << "Digite os " << tamanho << " elementos do vetor:\n";
+    for (int i = 0; i < tamanho; ++i) {
+        cout << "Elemento " << i + 1 << ": ";
+        cin >> vetorOriginal[i];
+    }
+
+    for (int i = 0; i < tamanho; ++i) {
+        if (vetorOriginal[i] == 0) {
+            vetorModificado[i] = 1;
+        } else {
+            vetorModificado[i] = vetorOriginal[i];
+        }
+    }
+
+    cout << "\nVetor original:\n";
+    for (int i = 0; i < tamanho; ++i) {
+        cout << vetorOriginal[i] << " ";
+    }
+
+    cout << "\n\nVetor modificado (substituindo 0 por 1):\n";
+    for (int i = 0; i < tamanho; ++i) {
+        cout << vetorModificado[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
