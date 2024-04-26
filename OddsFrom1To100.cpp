f6181f8425f19b88490d0b1a242d676831e72163
Levy Abreu <148262651+LevyAbreu@users.noreply.github.com>
2024-04-25 23:01:58 -0400

    Create OddsFrom1To100.cpp

---
+++ OddsFrom1To100.cpp
@@ -0,0 +1,26 @@
+//10) Escreva um algoritmo que armazene em um vetor todos os números ímpares do intervalo fechado de 1 a 100. Após isso, o algoritmo deve imprimir todos os valores armazenados.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int tamanho = 50;
+    int vetor[tamanho];
+    int contador = 0;
+
+    for (int i = 1; i <= 100; ++i) {
+        if (i % 2 != 0) {
+            vetor[contador] = i;
+            contador++;
+        }
+    }
+
+    cout << "Valores armazenados no vetor (números ímpares de 1 a 100):\n";
+    for (int i = 0; i < contador; ++i) {
+        cout << vetor[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
