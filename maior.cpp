1c93a5abd6610b8b7e31d057706be6f36638a8e1
Levy Abreu <148262651+LevyAbreu@users.noreply.github.com>
2024-04-25 22:27:08 -0400

    maior.cpp

--- maior.cpp
+++ maior.cpp
@@ -1,18 +1,31 @@
-int main(){
-int v[80],menor,i;
-for(i=1;i<=80;i++){
-    v[i]=i;
-}
-menor=v[1];
-for(i=2;i<=80;i++){
-    if(v[i]<menor){
-        menor=v[i];
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int tamanho = 80;
+    int vetor[tamanho];
+
+    // Lendo os elementos do vetor
+    cout << "Digite os " << tamanho << " elementos do vetor:\n";
+    for (int i = 0; i < tamanho; ++i) {
+        cout << "Elemento " << i + 1 << ": ";
+        cin >> vetor[i];
     }
-}
-for(i=1;i<=80;i++){
-    if(v[i]==menor){
-        printf('O menor elemento e: %d. Na posicao: %d.', v[i], i);
+
+    // Encontrando o menor elemento e sua posição
+    int menor = vetor[0];
+    int posicao = 0;
+    for (int i = 1; i < tamanho; ++i) {
+        if (vetor[i] < menor) {
+            menor = vetor[i];
+            posicao = i;
+        }
     }
-}
-return 0;
+
+    // Mostrando o menor elemento e sua posição
+    cout << "O menor elemento do vetor é: " << menor << endl;
+    cout << "Sua posição no vetor é: " << posicao + 1 << endl;
+
+    return 0;
 }
