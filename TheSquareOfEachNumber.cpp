f8cc196377b2c9f3d6b3f4937eec2220b3fbd327
Levy Abreu <148262651+LevyAbreu@users.noreply.github.com>
2024-04-25 23:05:01 -0400

    Create TheSquareOfEachNumber.cpp

---
+++ TheSquareOfEachNumber.cpp
@@ -0,0 +1,24 @@
+//12) Escreva um algoritmo que receba dez números do usuário e armazene em um vetor o quadrado de cada número. Após isso, o algoritmo deve imprimir todos os valores armazenados.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int tamanho = 10;
+    double vetor[tamanho];
+
+    for (int i = 0; i < tamanho; ++i) {
+        cout << "Digite o " << i + 1 << "o número: ";
+        cin >> vetor[i];
+        vetor[i] *= vetor[i];
+    }
+
+    cout << "\nValores armazenados no vetor (quadrado de cada número digitado):\n";
+    for (int i = 0; i < tamanho; ++i) {
+        cout << vetor[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
